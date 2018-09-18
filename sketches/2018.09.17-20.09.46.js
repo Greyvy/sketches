f9be8cbf93f5d8da8fa33919e06805edeb59e958
greyvy <greyvy@gmail.com>
2018-09-18 12:52:57 -0700

    Completed first draft of flipping code using /

--- sketches/2018.09.17-20.09.46.js
+++ sketches/2018.09.17-20.09.46.js
@@ -1,11 +1,17 @@
 let canvasSketch = require('canvas-sketch')
 
 let settings = {
+    animate: true,
+    duration: 4,
     dimensions: [ 1024, 1024 ]
 }
 
 let sketch = () => {
-    return ({ context: ctx, width, height }) => {
+
+    const PI = Math.PI
+    const TAU = PI * 2
+
+    return ({ context: ctx, width, height, playhead }) => {
         ctx.fillStyle = 'white'
         ctx.fillRect(0, 0, width, height)
 
@@ -14,13 +20,15 @@ let sketch = () => {
 
         let n = 4
         for (let i = 0; i <= n; ++i) {
+            let t = Math.sin(playhead * PI)
             ctx.fillStyle = 'hsla(0, 0%, 0%, 1)'
-            ctx.fillRect(width * 0.125 * i, height * 0.125 * i, 16, 16)
+            ctx.fillRect(width * 0.125 * i * t, height * 0.125 * i, 16, 16)
         }
 
+
+        // @NOTE(Grey) First flip, horizontal to the right half
         let d = ctx.getImageData(0, 0, width * 0.5, height * 0.5)
         let f = ctx.createImageData(d)
-
         for (let i = 0; i < d.data.length; i += 4) {
             let w = d.width * 4
             let y = Math.floor(i / w)
@@ -37,14 +45,25 @@ let sketch = () => {
 
         ctx.putImageData(f, width / 2, 0)
 
-
+        // @NOTE(Grey) Second flip, whol top half to bottom half
         let t = ctx.getImageData(0, 0, width, height * 0.5)
         let r = ctx.createImageData(t)
         for (let i = 0; i < t.data.length; i += 4) {
             let h = d.height * 4
-            let y = Math.floor(i / w)
+            let y = (t.data.length / h) - Math.floor(i / h)
+
+            let p0 = ((y * h) - ((i + 0) % h)) - 1
+            let p1 = ((y * h) - ((i + 1) % h)) - 1
+            let p2 = ((y * h) - ((i + 2) % h)) - 1
+            let p3 = ((y * h) - ((i + 3) % h)) - 1
+
+            r.data[i + 0] = t.data[p3]
+            r.data[i + 1] = t.data[p2]
+            r.data[i + 2] = t.data[p1]
+            r.data[i + 3] = t.data[p0]
         }
 
+        ctx.putImageData(r, 0, height / 2)
 
 
     }
