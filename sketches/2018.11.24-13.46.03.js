2b9d85f1a4c3fb2da31698f2a545b5df9a3ef56f
greyvy <greyvy@gmail.com>
2018-11-25 08:52:03 -0800

    Second pass

--- sketches/2018.11.24-13.46.03.js
+++ sketches/2018.11.24-13.46.03.js
@@ -8,15 +8,20 @@ let settings = {
     dimensions: [ 1024, 1024 ]
 }
 
-let sketch = ({ width, height }) => {
+let sketch = ({ context, width, height }) => {
 
     const PI = Math.PI
     const TAU = PI * 2
+    const seed_value = 42
 
-    let seed_value = 42
-
+    let clamp = (v, min, max) => v < min ? min : v > max ? max : v
+    let lerp = (v0, v1, t) => (1-t)*v0+t*v1
+    let map = (v, ds, de, rs, re) => rs+(re-rs)*((v-ds)/(de-ds))
     let col = (h=0, s=0, l=0, a=1) => `hsla(${h*360}, ${s*100}%, ${l*100}%, ${a})`
     let rand = seed(seed_value)
+
+
+    let vlerp = (v0, v1, t) => [lerp(v0[0], v1[0], t), lerp(v0[1], v1[1], t)]
 
     let circle = (ctx, x, y, r) => {
         ctx.beginPath()
@@ -24,73 +29,118 @@ let sketch = ({ width, height }) => {
         ctx.closePath()
     }
 
-    let particle_update = (ctx, playhead, part) => {
-        let pos = part.pos
-        let acc = part.acc
-        let vel = part.vel
-        let positions = part.positions
-
-        if (pos[0] <= 0)      { vel = vec.normal(vec.scale(vec.norm(vel), 5)) }
-        if (pos[0] >= width)  { vel = vec.normal(vec.scale(vec.norm(vel), 5)) }
-        if (pos[1] <= 0)      { vel = vec.normal(vec.scale(vec.norm(vel), 5)) }
-        if (pos[1] >= height) { vel = vec.normal(vec.scale(vec.norm(vel), 5)) }
-        let spd = vec.scale(vec.norm(vel), acc)
-        vel = vec.add(vec.rotate(vel, Math.sin(playhead*TAU)*0.05), spd)
-        pos = vec.add(pos, vel)
-        // vel = vec.scale(vec.rotate(vel, 0), 1)
-        positions.push([...pos, vec.mag(vel)])
-
-        ctx.save()
-        ctx.beginPath()
-        for (let i = 0; i < positions.length; ++i) {
-            let p = [positions[i][0], positions[i][1]]
-            let r = positions[i][2]
-            let t = vec.add(p, vec.scale(vec.normal(vec.norm(p)), r))
-            if (i === 0) {
-                ctx.moveTo(...t)
-            } else {
-                ctx.lineTo(...t)
-            }
-        }
-        ctx.stroke()
-        ctx.restore()
-
-        ctx.save()
-        ctx.beginPath()
-        for (let i = 0; i < positions.length; ++i) {
-            let p = [positions[i][0], positions[i][1]]
-            let r = positions[i][2]
-            let t = vec.add(p, vec.scale(vec.normal(vec.norm(p)), -r))
-            if (i === 0) {
-                ctx.moveTo(...t)
-            } else {
-                ctx.lineTo(...t)
-            }
-        }
-        ctx.stroke()
-        ctx.restore()
-
-        part.pos = pos
-        part.vel = vel
+    let debug_draw_points = (ctx, c) => {
+        ctx.fillStyle = col(0, 0, 0.125)
+        circle(ctx, ...c, 8)
+        ctx.fill()
     }
 
-    let particle = {
-        vel: [-5, 0],
-        acc: 0.05,
-        pos: [width/2, height/2],
-        positions: []
+    let curve_points = (res, v0, v1, v2) => {
+        let result = []
+        for (let i = 0; i <= res; ++i) {
+            let t = i/res
+            let a = vlerp(v0, v1, t)
+            let b = vlerp(v1, v2, t)
+            let c = vlerp(a, b, t)
+            result.push(c)
+        }
+        return result
     }
 
-    return ({ context, playhead }) => {
+    let curve_wobble = (playhead, c, i) => {
+        let r = i+playhead*(i%2 === 0 ? TAU : -TAU)*(1+i)
+        return vec.add(vec.rotate(vec.scale(vec.norm(c), 60), r), c)
+    }
+
+    let curve_draw = (ctx, cp, i, arr) => {
+        if (i === 0) {
+            ctx.beginPath()
+            ctx.strokeStyle = col(0, 0, 1)
+            ctx.moveTo(...cp)
+        } else if (i === arr.length-1) {
+            ctx.lineTo(...cp)
+            ctx.stroke()
+            // ctx.fill()
+        } else {
+            ctx.lineTo(...cp)
+        }
+    }
+
+    let curve_draw_points = (ctx, cp) => {
+        ctx.fillStyle = col(0, 0, 1)
+        circle(ctx, ...cp, 2)
+        ctx.fill()
+    }
+
+    let prepare_geometry = (res, arr) => {
+        let result = curve_points(res, arr[0], arr[1], arr[2])
+        return result
+    }
+
+
+    let ctx_debug_draw_points = debug_draw_points.bind(this, context)
+    let ctx_curve_draw_points = curve_draw_points.bind(this, context)
+    let ctx_curve_draw = curve_draw.bind(this, context)
+
+    /*
+    let eye = [
+        [width*0.25, height*0.5],
+        [width*0.5, height*0.25],
+        [width*0.75, height*0.5],
+        [width*0.5, height*0.75]
+    ]
+
+    let slit = [
+        [width*0.5, height*0.25],
+        [width*0.25, height*0.5],
+        [width*0.5, height*0.75],
+        [width*0.75, height*0.5]
+    ]
+    */
+
+    let curve = [
+        [width*0.25, height*0.5],
+        [width*0.5, height*0.25],
+        [width*0.75, height*0.5]
+    ]
+
+    return ({ playhead }) => {
         context.fillStyle = col()
         context.fillRect(0, 0, width, height)
 
-        context.fillStyle = col(0.5, 0.5, 0.5)
-        context.font = '18px monospace'
-        context.fillText(vec.mag(particle.vel), 16, 18+16)
-        context.strokeStyle = col(0, 0, 1)
+        // let curve0 = eye.map(curve_wobble.bind(this, playhead))
+        // let curve1 = slit.map(curve_wobble.bind(this, playhead))
 
-        particle_update(context, playhead, particle)
+        /*
+        let geo = [
+            [curve0[0], curve0[1], curve0[2]],
+            [curve0[0], curve0[3], curve0[2]],
+            [curve1[0], curve1[1], curve1[2]],
+            [curve1[0], curve1[3], curve1[2]]
+        ].map(prepare_geometry.bind(this, 32))
+        */
+
+        let geo = [curve].map(prepare_geometry.bind(this, 32))
+
+        // context.fillStyle = col(0, 0, 1)
+        geo.forEach((curve) => {
+            // curve.forEach(ctx_curve_draw_points)
+            curve.forEach(ctx_curve_draw)
+        })
+
+        /*
+        let geo = [
+            [curve0[0], curve0[1], curve0[2]],
+            [curve0[0], curve0[3], curve0[2]],
+            [curve0[1], curve0[2], curve0[3]],
+            [curve0[3], curve0[0], curve0[1]]
+        ].map(prepare_geometry.bind(this, 32))
+        */
+
+        /*
+        curve0.forEach(ctx_debug_draw_points)
+        curve1.forEach(ctx_debug_draw_points)
+        */
 
     }
 }
