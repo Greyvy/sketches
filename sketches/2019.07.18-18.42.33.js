fb3392e7ca4104795cfd727bc434ee3a3311058f
greyvy <greyvy@gmail.com>
2019-07-18 21:12:43 -0300

    Fridays sketch

---
+++ sketches/2019.07.18-18.42.33.js
@@ -0,0 +1,425 @@
+let canvasSketch = require('canvas-sketch')
+let SimplexNoise = require('simplex-noise')
+let Tone = require('tone')
+let load = require('load-asset')
+let create_regl = require('regl')
+let icosphere_make = require('primitive-icosphere')
+let cube_make = require('geo-3d-box')
+let plane_make = require('primitive-plane')
+let hsluv = require('hsluv')
+let seed = require('seed-random')
+let mat4 = require('gl-mat4')
+let vec3 = require('gl-vec3')
+
+let settings = {
+    context: 'webgl',
+    animate: true,
+    duration: 8.78,
+    dimensions: [ 1024, 1024 ],
+    attributes: {
+        antialiase: true
+    }
+}
+
+let sketch = async ({ gl, width, height }) => {
+
+    const PI = Math.PI
+    const TAU = PI * 2
+    const N = 128
+
+    let clamp = (v, min, max) => v < min ? min : v > max ? max : v
+    let lerp = (v0, v1, t) => (1-t)*v0+t*v1
+    let map = (v, ds, de, rs, re) => rs+(re-rs)*((v-ds)/(de-ds))
+    let ease = (p, g) => {
+      if (p < 0.5)
+        return 0.5 * Math.pow(2*p, g)
+      else
+        return 1 - 0.5 * Math.pow(2*(1 - p), g)
+    }
+
+    let seed_value = Math.floor(Math.random()*1000)
+    let rand = seed(seed_value)
+    let simplex = new SimplexNoise(seed_value)
+    let load_sound = function(str) {
+        return new Promise(function(resolve, reject) {
+            new Tone.Player(str, function(player) {
+                resolve(player)
+            })
+        })
+    }
+
+    let regl = create_regl({
+        gl,
+        extensions: ['webgl_draw_buffers', 'oes_texture_float', 'oes_standard_derivatives']
+    })
+
+    let text_make = function(colour) {
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        let fs = 200
+
+        canvas.width = width
+        canvas.height = height
+        ctx.fillStyle = 'hsla(0, 0%, 100%, 1)'
+        ctx.fillRect(0, 0, width, height)
+        ctx.fillStyle = 'hsla(0, 0%, 0%, 1)'
+        ctx.font = `${fs}px TrajanPro-Bold,sans-serif`
+        ctx.textAlign = 'center'
+        for (let i = 0; i < 30; ++i) {
+            ctx.fillText('TRUANT', canvas.width/2, fs*i)
+        }
+
+        return regl.texture({ data: canvas, wrapS: 'repeat', wrapT: 'repeat' })
+    }
+
+
+
+    let plane = plane_make(1, 1, 32, 32)
+    let text = text_make()
+
+    let bgc = hsluv.hsluvToRgb([rand()*360, rand()*100, rand()*100])
+
+    let plane_colour = hsluv.hsluvToRgb([rand()*360, rand()*100, rand()*100])
+    let plane_colours = plane.cells.map((v, i, a) => {
+        return [plane_colour, plane_colour, plane_colour]
+    })
+
+    let plane_fft = regl.texture({
+        shape: [N/4, N/4, 4],
+        min: 'linear',
+        mag: 'linear',
+        wrapS: 'repeat',
+        wrapT: 'repeat'
+    })
+
+
+    let plane_draw = regl({
+        frag: `
+        #ifdef GL_OES_standard_derivatives
+        #extension GL_OES_standard_derivatives : enable
+        #endif
+        precision mediump float;
+        #define PI 3.141592653589793
+
+        mat2 myt = mat2(0.12121212, 0.13131313, -0.13131313, 0.12121212);
+        vec2 mys = vec2(1e4, 1e6);
+
+        uniform float u_time;
+        uniform vec3 u_random;
+        uniform vec2 u_resolution;
+        uniform sampler2D u_text, u_fft;
+
+        varying float v_depth;
+        varying vec2 v_uv;
+        varying vec3 v_normal, v_vertex, v_color;
+
+        vec2 rhash(vec2 uv) {
+            uv *= myt;
+            uv *= mys;
+            return fract(fract(uv/mys)*uv);
+        }
+
+        vec3 hash(vec3 p) {
+            return fract(sin(vec3(dot(p, vec3(1.0, 57.0, 113.0)),
+                                  dot(p, vec3(57.0, 113.0, 1.0)),
+                                  dot(p, vec3(113.0, 1.0, 57.0))))*
+                        43758.5453);
+        }
+
+        float voronoi2d(in vec2 point) {
+            vec2 p = floor(point);
+            vec2 f = fract(point);
+            float res = 0.0;
+            for (int j = -1; j <= 1; j++) {
+                for (int i = -1; i <= 1; i++) {
+                    vec2 b = vec2(i, j);
+                    vec2 r = vec2(b)-f+rhash(p+b);
+                    res += 1.0/pow(dot(r, r), 8.0);
+                }
+            }
+            return pow(1.0/res, 0.0625);
+        }
+
+        vec3 voronoi3d(in vec3 x) {
+            vec3 p = floor(x);
+            vec3 f = fract(x);
+
+            float id = 0.0;
+            vec2 res = vec2(100.0);
+            for (int k = -1; k <= 1; k++) {
+                for (int j = -1; j <= 1; j++) {
+                    for (int i = -1; i <= 1; i++) {
+                        vec3 b = vec3(float(i), float(j), float(k));
+                        vec3 r = vec3(b)-f+hash(p+b);
+                        float d = dot(r, r);
+
+                        float cond = max(sign(res.x-d), 0.0);
+                        float nCond = 1.0-cond;
+
+                        float cond2 = nCond*max(sign(res.y-d), 0.0);
+                        float nCond2 = 1.0-cond2;
+
+                        id = (dot(p+b, vec3(1.0, 57.0, 113.0))*cond)+(id*nCond);
+                        res = vec2(d, res.x)*cond+res*nCond;
+
+                        res.y = cond2*d+nCond2*res.y;
+                    }
+                }
+            }
+            return vec3(sqrt(res), abs(id));
+        }
+
+        mat2 rotate2d(float angle) {
+            return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+        }
+
+        float map_range(float v, float ds, float de, float rs, float re) {
+            return rs+(re-rs)*((v-ds)/(de-ds));
+        }
+
+        void main() {
+            vec2 st = gl_FragCoord.xy/u_resolution.xy;
+            vec2 uv = v_uv;
+
+            vec2 uv_t = uv;
+
+            // uv_t.y += pow(sin(u_time*PI)*uv_t.y, st.y);
+            // uv_t.x = uv_t.x+1.0-pow(abs(u_time), 1.0);
+            // uv_t.y = mod(uv_t.y, 0.5);
+
+            vec4 fft = texture2D(u_fft, uv_t);
+            uv_t += fft.xy*0.025;
+            vec4 text = texture2D(u_text, uv_t);
+            vec4 color = vec4(v_color, 1.0);
+            // vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
+
+            text.rgb = text.r < 1.0 ? fft.grr : vec3(1.0);
+            // text.rgb *= fft.gbr;
+
+            color = color*text;
+
+            vec3 U = dFdx(v_vertex);
+            vec3 V = dFdy(v_vertex);
+            vec3 normal = normalize(cross(U,V));
+            vec3 light_direction = normalize(
+                vec3(0.2, 0.3, 0.7)*u_random
+            );
+            float light_intensity = dot(normal, light_direction);
+
+            color.rgb *= light_intensity;
+
+            gl_FragColor = vec4(color);
+        }`,
+
+        vert: `
+        precision mediump float;
+        #define PI 3.141592653589793
+
+        mat2 myt = mat2(0.12121212, 0.13131313, -0.13131313, 0.12121212);
+        vec2 mys = vec2(1e4, 1e6);
+
+        attribute vec3 a_position, a_color, a_normal;
+        attribute vec2 a_uv;
+
+        uniform mat4 u_projection, u_view, u_matrix;
+        uniform vec3 u_random;
+        uniform vec2 u_resolution;
+        uniform float u_time;
+        uniform sampler2D u_fft;
+
+        varying float v_depth;
+        varying vec2 v_uv;
+        varying vec3 v_normal, v_vertex, v_color;
+
+        vec2 rhash(vec2 uv) {
+            uv *= myt;
+            uv *= mys;
+            return fract(fract(uv/mys)*uv);
+        }
+
+        vec3 hash(vec3 p) {
+            return fract(sin(vec3(dot(p, vec3(1.0, 57.0, 113.0)),
+                                  dot(p, vec3(57.0, 113.0, 1.0)),
+                                  dot(p, vec3(113.0, 1.0, 57.0))))*
+                        43758.5453);
+        }
+
+        float voronoi2d(in vec2 point) {
+            vec2 p = floor(point);
+            vec2 f = fract(point);
+            float res = 0.0;
+            for (int j = -1; j <= 1; j++) {
+                for (int i = -1; i <= 1; i++) {
+                    vec2 b = vec2(i, j);
+                    vec2 r = vec2(b)-f+rhash(p+b);
+                    res += 1.0/pow(dot(r, r), 8.0);
+                }
+            }
+            return pow(1.0/res, 0.0625);
+        }
+
+        vec3 voronoi3d(in vec3 x) {
+            vec3 p = floor(x);
+            vec3 f = fract(x);
+
+            float id = 0.0;
+            vec2 res = vec2(100.0);
+            for (int k = -1; k <= 1; k++) {
+                for (int j = -1; j <= 1; j++) {
+                    for (int i = -1; i <= 1; i++) {
+                        vec3 b = vec3(float(i), float(j), float(k));
+                        vec3 r = vec3(b)-f+hash(p+b);
+                        float d = dot(r, r);
+
+                        float cond = max(sign(res.x-d), 0.0);
+                        float nCond = 1.0-cond;
+
+                        float cond2 = nCond*max(sign(res.y-d), 0.0);
+                        float nCond2 = 1.0-cond2;
+
+                        id = (dot(p+b, vec3(1.0, 57.0, 113.0))*cond)+(id*nCond);
+                        res = vec2(d, res.x)*cond+res*nCond;
+
+                        res.y = cond2*d+nCond2*res.y;
+                    }
+                }
+            }
+            return vec3(sqrt(res), abs(id));
+        }
+
+        float map_range(float v, float ds, float de, float rs, float re) {
+            return rs+(re-rs)*((v-ds)/(de-ds));
+        }
+
+        float quadratic_in_out(float t) {
+            float p = 2.0*t*t;
+            return t < 0.5 ? p : -p+(4.0*t)-1.0;
+        }
+
+        float wrap(float t) {
+            return 1.0-pow(abs((t*2.0)-1.0), 1.0);
+        }
+
+        void main() {
+
+            vec4 fake_frag_coord = u_matrix*vec4(a_position, 1.0);
+            fake_frag_coord.xyz /= fake_frag_coord.w;
+            fake_frag_coord.w = 1.0/fake_frag_coord.w;
+
+            fake_frag_coord.xyz *= vec3(0.5)+vec3(0.5);
+            fake_frag_coord.xy *= u_resolution.xy;
+
+            vec2 st = fake_frag_coord.xy/u_resolution.xy;
+            vec2 uv = a_uv;
+
+            vec4 norm = u_matrix*vec4(a_normal.xyz, 1.0);
+            vec4 fft = texture2D(u_fft, uv);
+            vec3 position = a_position;
+            float noise = voronoi2d((sin(uv*u_random.xy*PI))*16.0);
+            vec3 noise3 = voronoi3d(vec3(uv*u_random.xy, wrap(quadratic_in_out(u_time)))*3.0);
+
+            // position.xyz *= noise3;
+            position.z -= wrap(quadratic_in_out(u_time))*(noise*(length(fft)*0.25));
+
+            v_uv = a_uv;
+            v_color = a_color;
+            v_normal = vec3(norm.xyz);
+            v_depth = position.z;
+            v_vertex = (u_matrix*vec4(position, 1.0)).xyz;
+
+            gl_Position = u_projection*u_view*u_matrix*vec4(position, 1.0);
+        }`,
+
+        attributes: {
+            a_position: plane.positions,
+            a_uv: plane.uvs,
+            a_normal: plane.normals,
+            a_color: plane_colours,
+        },
+
+        elements: plane.cells,
+
+        uniforms: {
+            u_view: ({time}, props) => {
+                return mat4.lookAt([],
+                    [0.0, 0.0, 1.5], // position of camera
+                    [0.0, 0.0, 0.0], // point of view looking at
+                    [0.0, 1.0, 0.0]  // pointing up
+                )
+            },
+            u_matrix: (stats, props) => {
+                let { u_time, u_index, u_random, u_resolution } = props
+                let tra = mat4.translate([], mat4.identity([]), [
+                    (-3.0*u_index)+(Math.sin(ease(u_time, 1.5)*(PI*0.5))*6.0), 0, 0
+                    // 0, (3.0*u_index)+(Math.sin(ease(u_time, 1.5)*(PI*0.5))*-6.0), 0
+                    // (3.0*u_index)+(Math.sin(ease(u_time, 1.5)*(PI*0.5))*-6.0), 0, 0
+                ])
+                let rot = mat4.rotate([], tra, -PI, [1, 0, 0])
+                return mat4.scale([], rot, [1.5, 1.5, 1.5])
+            },
+            u_projection: ({viewportWidth, viewportHeight}) => {
+                return mat4.perspective([],
+                    PI/2, viewportWidth/viewportHeight, 0.01, 50)
+            },
+            u_time: regl.prop('u_time'),
+            u_random: regl.prop('u_random'),
+            u_resolution: regl.prop('u_resolution'),
+            u_position: regl.prop('u_position'),
+            u_text: regl.prop('u_text'),
+            u_fft: regl.prop('u_fft')
+        },
+        blend: {
+            enable: true,
+            func: {
+                srcRGB:   'src alpha',
+                srcAlpha: 'src alpha',
+                dstRGB:   'one minus src alpha',
+                dstAlpha: 'one minus src alpha'
+            }
+        },
+        primitive: 'triangles'
+    })
+
+    let player = await load_sound(`/assets/2019_07_19.mp3`)
+    let fft = new Tone.FFT(N*N)
+    let fft_r = new Uint8Array(N*N)
+    player.connect(fft)
+    player.toMaster()
+    player.autostart = true
+    player.loop = true
+
+    let plane_elements = new Array(3)
+        .fill({})
+        .map(function(value, i) {
+            return {
+                u_resolution: [width, height],
+                u_random: [rand(), rand(), rand()],
+                u_text: text,
+                u_index: i
+            }
+        })
+
+    return {
+        begin() {
+            player.restart()
+        },
+        render({ playhead }) {
+            regl.poll()
+            regl.clear({color: [...bgc, 1], depth: 1})
+
+            let fft_v = fft.getValue()
+            for (let i = 0; i < fft_v.length; ++i) {
+                fft_r[i] = Math.floor(map(fft_v[i], -10, 0, 0, 255))
+            }
+            plane_fft.subimage(fft_r)
+
+            plane_draw(plane_elements.map(function(value) {
+                return Object.assign(value, {u_time: playhead, u_fft: plane_fft })
+            }))
+        }
+    }
+}
+
+canvasSketch(sketch, settings)
+
+
